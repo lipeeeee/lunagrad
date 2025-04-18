8a975334e7c1eb316d24a1bf554cd59b7caa64e8
lipeeeee <filipe.pereirafga@gmail.com>
2025-04-18 20:57:41 +0100

    nn.py

---
+++ lunagrad/nn.py
@@ -0,0 +1,59 @@
+import random
+from lunagrad.engine import Value
+
+class Module:
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+class Neuron(Module):
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(0)
+
+    def __call__(self, x):
+        out = sum((wi*xi for wi,xi in zip(self.w, x))) + self.b
+        return out.relu()
+
+    def parameters(self):
+        return self.w + [self.b]
+
+    def __repr__(self):
+        return f"LinearNeuron({len(self.w)})"
+
+
+class Layer(Module):
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+class MLP(Module):
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
